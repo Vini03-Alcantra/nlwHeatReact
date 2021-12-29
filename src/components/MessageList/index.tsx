c126745dd28f6275a9bd0a9a2dbdbf4d5335022d
vinicius de alcântra Barros <viniciusalcantarabarros@gmail.com>
2021-12-29 13:44:53 -0300

    Renderizando mesnagens do backend

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,51 +1,50 @@
 import stylesSheet from "./styles.module.scss"
 
 import logoImg from '../../../assets/logo.svg'
+import { useEffect } from "react"
+import { api } from "../../services/api"
+import { useState } from "react"
+
+
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avat_url: string;
+    }
+}
 
 export function MessageList() {
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        api.get<Message[]>('messages/last3').then(res => {
+            setMessages(res.data)
+        })
+    }, [])
+
     return (
         <div className={stylesSheet.messageListWrapper}>
             <img src={logoImg} alt="DOWhile 2021" />
 
             <ul className={stylesSheet.messageList}>
-                <li className={stylesSheet.message}>
-                    <p className={stylesSheet.messageContent}>
-                        Não vejo a hora de começar o DoWhile
-                    </p>
+                {messages.map(message => {
+                    return (
+                        <li key={message.id} className={stylesSheet.message}>
+                            <p className={stylesSheet.messageContent}>
+                                {message.text}
+                            </p>
 
-                    <div className={stylesSheet.messageUser}>
-                        <div className={stylesSheet.userImage}>
-                            <img src="https://github.com/Vini03-Alcantra.png" alt="" />
-                        </div>                        
-                        <span>Vinícius de Alcântra</span>
-                    </div>
-                </li>
-
-                <li className={stylesSheet.message}>
-                    <p className={stylesSheet.messageContent}>
-                        Não vejo a hora de começar o DoWhile
-                    </p>
-
-                    <div className={stylesSheet.messageUser}>
-                        <div className={stylesSheet.userImage}>
-                            <img src="https://github.com/Vini03-Alcantra.png" alt="" />
-                        </div>                        
-                        <span>Vinícius de Alcântra</span>
-                    </div>
-                </li>
-
-                <li className={stylesSheet.message}>
-                    <p className={stylesSheet.messageContent}>
-                        Não vejo a hora de começar o DoWhile
-                    </p>
-
-                    <div className={stylesSheet.messageUser}>
-                        <div className={stylesSheet.userImage}>
-                            <img src="https://github.com/Vini03-Alcantra.png" alt="" />
-                        </div>                        
-                        <span>Vinícius de Alcântra</span>
-                    </div>
-                </li>
+                            <div className={stylesSheet.messageUser}>
+                                <div className={stylesSheet.userImage}>
+                                    <img src={message.user.avat_url} alt={message.user.name} />
+                                </div>                        
+                                <span>{message.user.name}</span>
+                            </div>
+                        </li>
+                    )
+                })}                
             </ul>
         </div>
     )
