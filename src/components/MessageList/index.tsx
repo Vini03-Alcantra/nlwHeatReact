8c2a0f1e90be35e1b4f4fa0eeb52dd1182cd8916
vinicius de alcântra Barros <viniciusalcantarabarros@gmail.com>
2021-12-29 12:25:23 -0300

    Organizando comentarios

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,52 @@
+import stylesSheet from "./styles.module.scss"
+
+import logoImg from '../../../assets/logo.svg'
+
 export function MessageList() {
     return (
-        <h1>Message List</h1>
+        <div className={stylesSheet.messageListWrapper}>
+            <img src={logoImg} alt="DOWhile 2021" />
+
+            <ul className={stylesSheet.messageList}>
+                <li className={stylesSheet.message}>
+                    <p className={stylesSheet.messageContent}>
+                        Não vejo a hora de começar o DoWhile
+                    </p>
+
+                    <div className={stylesSheet.messageUser}>
+                        <div className={stylesSheet.userImage}>
+                            <img src="https://github.com/Vini03-Alcantra.png" alt="" />
+                        </div>                        
+                        <span>Vinícius de Alcântra</span>
+                    </div>
+                </li>
+
+                <li className={stylesSheet.message}>
+                    <p className={stylesSheet.messageContent}>
+                        Não vejo a hora de começar o DoWhile
+                    </p>
+
+                    <div className={stylesSheet.messageUser}>
+                        <div className={stylesSheet.userImage}>
+                            <img src="https://github.com/Vini03-Alcantra.png" alt="" />
+                        </div>                        
+                        <span>Vinícius de Alcântra</span>
+                    </div>
+                </li>
+
+                <li className={stylesSheet.message}>
+                    <p className={stylesSheet.messageContent}>
+                        Não vejo a hora de começar o DoWhile
+                    </p>
+
+                    <div className={stylesSheet.messageUser}>
+                        <div className={stylesSheet.userImage}>
+                            <img src="https://github.com/Vini03-Alcantra.png" alt="" />
+                        </div>                        
+                        <span>Vinícius de Alcântra</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
