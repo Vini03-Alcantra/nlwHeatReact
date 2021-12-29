33334de39fc2b5ad3c9f99308f2e2d7509fb5bc7
vinicius de alcântra Barros <viniciusalcantarabarros@gmail.com>
2021-12-29 14:07:44 -0300

    Recebendo codigo do backend

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,11 +1,26 @@
+import { useEffect } from "react"
 import { VscGithubInverted } from "react-icons/vsc"
 import styles from './style.module.scss'
 
 export function LoginBox() {
+    const signinUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=f78b173c3922b4390349`
+
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGithubCode = url.includes('?code=')
+
+        if(hasGithubCode){
+            const [urlWithoutCode, githubCode] = url.split('?code=')
+
+            console.log({urlWithoutCode, githubCode})
+            window.history.pushState({}, '', urlWithoutCode)
+        }
+    }, [])
+
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
+            <a href={signinUrl} className={styles.signInWithGithub}>
                 <VscGithubInverted size="24" />
                 Entrar com github
             </a>
